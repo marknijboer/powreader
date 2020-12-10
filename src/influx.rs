47ca25f87ad617829aa8fecc7b2a327c7a0a291a
Mark Nijboer <mark@nijboer.email>
2020-12-11 00:17:29 +0100

    * Fixed influxdb data format

--- src/influx.rs
+++ src/influx.rs
@@ -18,14 +18,14 @@ pub fn send_stats(host: &str, data: &MeterOutput) {
 
     let point = point!("power")
     .add_field("actual_delivered_power", Value::Float(data.actual_delivered_power))
-    .add_field("actual_returned_power", Value::Float(data.actual_delivered_power))
-    .add_field("power_failures", Value::Float(data.actual_delivered_power))
-    .add_field("long_power_failures", Value::Float(data.actual_delivered_power))
-    .add_field("power_delivered_tariff1", Value::Float(data.actual_delivered_power))
-    .add_field("power_delivered_tariff2", Value::Float(data.actual_delivered_power))
-    .add_field("power_returned_tariff1", Value::Float(data.actual_delivered_power))
-    .add_field("power_returned_tariff2", Value::Float(data.actual_delivered_power))
-    .add_field("power_tariff_indicator", Value::Float(data.actual_delivered_power))
+    .add_field("actual_returned_power", Value::Float(data.actual_returned_power))
+    .add_field("power_failures", Value::Integer(data.power_failures as i64))
+    .add_field("long_power_failures", Value::Integer(data.long_power_failures as i64))
+    .add_field("power_delivered_tariff1", Value::Float(data.power_delivered_tariff1))
+    .add_field("power_delivered_tariff2", Value::Float(data.power_delivered_tariff2))
+    .add_field("power_returned_tariff1", Value::Float(data.power_returned_tariff1))
+    .add_field("power_returned_tariff2", Value::Float(data.power_returned_tariff2))
+    .add_field("power_tariff_indicator", Value::Integer(data.tariff_indicator as i64))
     .add_field("power_equipment_id", Value::String(data.power_equipment_id.clone()));
 
     udp.write_point(point).unwrap();
